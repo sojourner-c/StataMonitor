5e5cd207aa2a4b100f751894abc3b54b47101a51
Chris Sojourner <sojourner.c@ei.com>
2017-08-24 16:45:28 -0400

    Add tests for stata_monitor running without errors with correct call. Add tests that validate body of email text generated in stata_monitor for both good and bad .do files.

--- test/test_sm.py
+++ test/test_sm.py
@@ -1,22 +1,39 @@
 """Unit test for Stata Monitor"""
 
+import os
 import unittest
+
+from lib import stata_monitor
 
 
 class TestStataMonitor(unittest.TestCase):
 	"""Test stata_monitor.py"""
 
 	def setUp(self):
-		self.path = r'C:\Users\sojourner_c\AppData\Local\Programs\Python\Personal Projects\Stata_Monitor\test\do'
-		self.pass_file = 'pass_test.do'
-		self.fail_file = 'fail_test.do'
+		self.path = os.path.dirname(os.path.abspath(__file__)) + '\\do'
+		self.pass_file = self.path + '\\' + 'pass_test.do'
+		self.fail_file = self.path + '\\' + 'fail_test.do'
+		self.pass_body = 'The program pass_test.do completed without errors.'
+		self.fail_body = 'The program fail_test.do terminated due to errors.'
 
 
-	def test_no_rc(self):
-		"""Description"""
-		pass
+	def test_stata_monitor(self):
+		"""Test that stata_monitor runs without errors."""
+		stata_monitor.stata_monitor(self.pass_file)
+		stata_monitor.stata_monitor(self.fail_file)
 
 
-	def test_rc(self):
-		"""Description"""
-		pass
+	def test_body_no_rc(self):
+		"""Test _set_up, _run_program, and _scan_log run with good .do file."""
+		self.log_name_tup = stata_monitor._set_up(self.pass_file)
+		stata_monitor._run_program(self.pass_file)
+		body = stata_monitor._scan_log(self.log_name_tup)
+		self.assertEqual(body, self.pass_body)
+
+
+	def test_body_rc(self):
+		"""Test _set_up, _run_program, and _scan_log run with bad .do file."""
+		self.log_name_tup = stata_monitor._set_up(self.fail_file)
+		stata_monitor._run_program(self.fail_file)
+		body = stata_monitor._scan_log(self.log_name_tup)
+		self.assertEqual(body, self.fail_body)
